29d8a63834ecdaaadeeaf97d9c59ed319693d689
sergiovzambelli <sergiovzambelli@gmail.com>
2024-05-03 09:31:11 +0200

    Update, more testing

--- testing/dqn_test_array.py
+++ testing/dqn_test_array.py
@@ -1,4 +1,5 @@
 import random
+import time
 import gymnasium as gym
 import os
 from stable_baselines3 import DQN
@@ -12,8 +13,8 @@ from generator_test import TrafficGenerator
 
 
 
-nVehicles = 6000
-nTests = 50
+nVehicles = 2500
+nTests = 20
 
 csv_file_path = f'output_{nVehicles}/'
 tests = {
@@ -23,9 +24,25 @@ tests = {
 NUM_SECONDS = 28000
 ITERATIONS = 5400
 model_path = "../models/DQN_49ore/best_847800.zip" # Specificare il modello
+times_file_path = os.path.join(csv_file_path, 'times.txt')
 
 if not os.path.exists(csv_file_path):
     os.makedirs(csv_file_path)
+    
+constants_file_path = os.path.join(csv_file_path, 'constants.txt')
+
+with open(constants_file_path, 'a') as constants_file:
+    constants_file.write("Costanti inizializzate:\n\n")
+
+    # Salvataggio delle costanti
+    constants_file.write("tests:\n")
+    for test_name, test_values in tests.items():
+        constants_file.write(f"{test_name}: {test_values}\n")
+
+    constants_file.write(f"\nNUM_SECONDS: {NUM_SECONDS}\n")
+    constants_file.write(f"ITERATIONS: {ITERATIONS}\n")
+    constants_file.write(f"model_path: {model_path}\n")
+    constants_file.write(f"csv_file_path: {csv_file_path}\n")
     
 
 for output_file, values in tests.items():
@@ -49,26 +66,15 @@ for output_file, values in tests.items():
         if csv_file.tell() == 0:
             writer.writeheader()
 
-        for _ in range(ITERATIONS):
-            env.render()
-            action, _ = model.predict(obs)
-            obs, reward, done, _, info = env.step(action)
+        with open(times_file_path, 'a') as times_file:
+            for _ in range(ITERATIONS):
+                env.render()
+                start_time = time.time()
+                action, _ = model.predict(obs)
+                obs, reward, done, _, info = env.step(action)
+                end_time = time.time()
 
-            writer.writerow(info)
+                writer.writerow(info)
+                times_file.write(f"{end_time - start_time}\n")
 
     env.close()
-
-constants_file_path = os.path.join(csv_file_path, 'constants.txt')
-
-with open(constants_file_path, 'w') as constants_file:
-    constants_file.write("Costanti inizializzate:\n\n")
-
-    # Salvataggio delle costanti
-    constants_file.write("tests:\n")
-    for test_name, test_values in tests.items():
-        constants_file.write(f"{test_name}: {test_values}\n")
-
-    constants_file.write(f"\nNUM_SECONDS: {NUM_SECONDS}\n")
-    constants_file.write(f"ITERATIONS: {ITERATIONS}\n")
-    constants_file.write(f"model_path: {model_path}\n")
-    constants_file.write(f"csv_file_path: {csv_file_path}\n")
